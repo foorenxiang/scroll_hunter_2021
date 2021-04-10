5566a5a53ccaf279a9498f49678baf98cc7bf987
Ren Xiang <foorenxiang@gmail.com>
2021-04-10 16:57:19 +0800

    updated page title

--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@
 // note that js extension is required for browser
 import { fullPageRender } from './MainRenderers.js';
 
+const setPageTitle = () => {
+  document.title = 'Scroll Hunter 2021';
+};
+
+setPageTitle();
 fullPageRender();
